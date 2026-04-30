330d25a3f529d3261e90d2ec89a0d88d802f6a70
Sakshi <sakshi.singh_cs22@gla.ac.in>
2026-05-01 00:43:04 +0530

    Added StudentFeeDiscount

---
+++ CoreProgramming_Basic/Programming_Elements/Level-1/StudentFeeDiscount.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class StudentFeeDiscount {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Student Fee: ");
+        double fee = sc.nextDouble();
+        System.out.print("Enter Discount Percentage: ");
+        double discountPercent = sc.nextDouble();
+        double discount = (fee * discountPercent) / 100;
+        double finalFee = fee - discount;
+        System.out.println("The discount amount is INR " + discount);
+        System.out.println("The final discounted fee is INR " + finalFee);
+    }
+}
